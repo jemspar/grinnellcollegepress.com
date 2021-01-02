3619e8c18ffdfcc0dbe8550687bb5539ad63041f
Jeremy Sparagon <j.a.sparagon@outlook.com>
2021-01-01 21:44:01 -0800

    menu working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@ import React from 'react'
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuModalOpen: false,
+      subtitle: false
+    }
+  }
+
+  toggleMenu() {
+    this.setState({menuModalOpen: !this.state.menuModalOpen});
+  }
+
 
   render() {
+
     return(
-      <h1>Grinnell College Press</h1>
+      <>
+        <div id="content">
+          <h1 id="title">Grinnell College Press</h1>
+          <button onClick={() => this.toggleMenu()}>
+            Open Menu
+          </button>
+        </div>
+
+        {this.state.menuModalOpen &&
+          <div id="menu_modal">
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>Publications</li>
+                <li>About</li>
+              </ul>
+            </nav>
+            <button onClick={() => this.toggleMenu()}>Close Menu</button>
+          </div>
+        }
+      </>
+
     )
   }
 }
