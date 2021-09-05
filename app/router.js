a6eef0e37b043ef465c6e365d57050db858f4ccc
Jeremy Sparagon <j.a.sparagon@outlook.com>
2021-09-05 13:12:34 -0700

    about and submissions pg

--- app/router.js
+++ app/router.js
@@ -8,5 +8,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('about');
-  this.route('contact', { path: '/getting-in-touch' });
+  this.route('submissions');
 });
